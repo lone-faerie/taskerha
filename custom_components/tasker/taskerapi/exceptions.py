f7afad4fe94082b44b3d4c41237faff8bee1c8f2
lone-faerie <lone.faerie@gmail.com>
2023-05-31 02:38:41 -0700

    removed taskerapi

--- custom_components/tasker/taskerapi/exceptions.py
+++
@@ -1,6 +0,0 @@
-"""Exceptions for taskerapi"""
-class TaskerError(Exception):
-    """Base exception for taskerapi"""
-    
-class TaskerAuthError(TaskerError):
-    """Represent Tasker authorization errors"""
